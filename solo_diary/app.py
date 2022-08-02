1988e175136211bd98343b9974abf1019eab3927
sun2525 <ghd7673@naver.com>
2022-08-02 09:31:25 +0900

    upgrade solo_diary

--- solo_diary/app.py
+++ solo_diary/app.py
@@ -1,50 +1,50 @@
+
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
+# client = MongoClient('localhost', 27017)
 client = MongoClient('mongodb+srv://test:1234@cluster0.sh1ym.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+db = client.dbsparta_plus_week1
 
-## HTML을 주는 부분
+from datetime import datetime
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/memo', methods=['GET'])
-def listing():
-    articles = list(db.articles.find({}, {'_id': False}))
-    return jsonify({'all_articles':articles})
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    diaries = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'all_diary': diaries})
 
-## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def saving():
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
+    file = request.files["file_give"]
 
-    soup = BeautifulSoup(data.text, 'html.parser')
+    extension = file.filename.split('.')[-1]
 
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-    desc = soup.select_one('meta[property="og:description"]')['content']
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    filename = f'file-{mytime}'
+
+    save_to = f'static/{filename}.{extension}'
+    file.save(save_to)
 
     doc = {
-        'title':title,
-        'image':image,
-        'desc':desc,
-        'url':url_receive,
-        'comment':comment_receive
+        'title':title_receive,
+        'content':content_receive,
+        'file': f'{filename}.{extension}',
+        'time': today.strftime('%Y.%m.%d')
     }
 
-    db.articles.insert_one(doc)
+    db.diary.insert_one(doc)
 
-    return jsonify({'msg':'저장이 완료되었습니다!'})
+    return jsonify({'msg': '저장 완료!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
